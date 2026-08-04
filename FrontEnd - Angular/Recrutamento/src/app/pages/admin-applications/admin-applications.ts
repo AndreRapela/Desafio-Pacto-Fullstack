4b3c78909306fe55604c30059df9f77018a1a1a7
Andre Rapela <andre.d.q.rapela@gmail.com>
2026-08-04 01:41:52 -0300

    componentes angular imlementados

---
+++ FrontEnd - Angular/Recrutamento/src/app/pages/admin-applications/admin-applications.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-applications',
+  imports: [],
+  templateUrl: './admin-applications.html',
+  styleUrl: './admin-applications.css',
+})
+export class AdminApplications {}
